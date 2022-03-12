dce39350e807ed95dd60a0fd03fade8bbd444c67
Arun <arungawande1995@gmail.com>
2022-03-12 18:28:28 +0530

    Arun: Added  ReverseTheNumber Program

---
+++ ReverseTheNumber.java
@@ -0,0 +1,24 @@
+package Day6_Logical_Program;
+
+import java.util.Scanner;
+
+public class ReverseTheNumber
+{
+    public static void main(String[] args) {
+
+        Scanner scannerObject = new Scanner(System.in);
+        int remainder, reversedNumber = 0;
+        System.out.println("Enter the number to be reversed:");
+        int numberToReverse = scannerObject.nextInt();
+        scannerObject.close();
+
+        while (numberToReverse > 0) {
+
+            remainder = numberToReverse % 10;
+            reversedNumber = (reversedNumber * 10) + remainder;
+            numberToReverse = numberToReverse / 10;
+        }
+
+        System.out.println("The reversed Number is: " + reversedNumber);
+    }
+}
